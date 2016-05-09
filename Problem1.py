52532ec67bb7670f8bffffd7defaa3caf86ad40c
Girish <girishkumarkh@gmail.com>
2016-05-09 17:31:23 +0100

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -3,9 +3,9 @@
 # Problem 1 (Status : Success)
 
 sum = 0
-	for i in range (1,1000): 
-		if i%3==0 or i%5==0:
-			sum = sum + i
-	print sum
+for i in range (1,1000): 
+	if i%3==0 or i%5==0:
+		sum = sum + i
+print sum
 
 # Output : 233168
