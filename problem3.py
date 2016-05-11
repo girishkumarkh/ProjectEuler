4e00117b17cc8fffad93c9feaf76405e38a42895
girishkumarkh <girishkumarkh@gmail.com>
2016-05-11 22:03:57 +0100

    readd

---
+++ problem3.py
@@ -0,0 +1,16 @@
+# Project Euler solutions by Girish 
+
+# Problem 3 (Status : Incomplete)
+
+class problem3:  # incomplete 
+	def __init__(self):
+		print "problem 3"
+
+	def prime_number:
+		pass
+		
+
+	#i = 0
+	#while i<=600851475143:
+
+# Output : --
